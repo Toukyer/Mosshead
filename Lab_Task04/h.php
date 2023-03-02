1c134301a091d23dcad83d6739abfa5599301921
Toukyer <110295363+Toukyer@users.noreply.github.com>
2023-03-02 23:52:36 +0600

    Add files via upload

---
+++ Lab_Task04/h.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+
+</head>
+<body>
+
+<h1 style="text-align:center;"> X-Company</h1> 
+
+<div style="text-align:right;">
+
+<a href="h.php"><button>Home</button></a>
+<a href="login.php"><button>Login</button></a>
+<a href="signup.php"><button>Registration</button></a>
+
+</div>
+
+<h1 style="text-align:center;">Welcome to Xcompany</h1>
+
+</body>
+</html>
